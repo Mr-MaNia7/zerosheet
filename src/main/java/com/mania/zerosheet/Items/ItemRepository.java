9055e456c925fe44a97db519b4f16be971e557eb
Mr-MaNia7 <93995267+Mr-MaNia7@users.noreply.github.com>
2022-04-27 22:39:32 +0300

    findByName

--- src/main/java/com/mania/zerosheet/Items/ItemRepository.java
+++ src/main/java/com/mania/zerosheet/Items/ItemRepository.java
@@ -1,8 +1,11 @@
 package com.mania.zerosheet.Items;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends 
 CrudRepository<Item, Long>{
-     
+    List<Item> findByItemName(String itemName);
 }
