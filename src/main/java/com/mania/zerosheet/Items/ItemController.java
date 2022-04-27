3ba62767eee58828ccd05e9cd5739c476c87767e
Mr-MaNia7 <93995267+Mr-MaNia7@users.noreply.github.com>
2022-04-27 22:38:44 +0300

    Add more mappings

--- src/main/java/com/mania/zerosheet/Items/ItemController.java
+++ src/main/java/com/mania/zerosheet/Items/ItemController.java
@@ -1,9 +1,13 @@
 package com.mania.zerosheet.Items;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ItemController {
@@ -17,5 +21,56 @@ public class ItemController {
     public String showItemsPage(Item item, Model model){
         model.addAttribute("items", itemRepository.findAll());
         return "items";
+    }//
+    @GetMapping("/newitem")
+    public String showItemAddForm(Item item){
+        return "add-item";
+    }//
+    // from items page
+    @GetMapping(value="/edititem/{itemId}")
+    public String showUpdateItemForm(@PathVariable("itemId") long itemId, @Valid Item item, 
+    BindingResult result, Model model) {
+        if (result.hasErrors()){
+            item.setItemId(itemId);
+            return "update-item";
+        }
+        itemRepository.save(item);
+        model.addAttribute("items", itemRepository.findAll());
+        return "items";
     }
+    // from items page
+    @GetMapping(value="/deleteitem/{itemId}")
+    public String deleteItem(@PathVariable("itemId") long itemId, Model model) {
+        Item item = 
+            itemRepository
+                .findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid item Id: " + itemId));
+    itemRepository.delete(item);
+    model.addAttribute("items", itemRepository.findAll());
+    return "items";
+    }
+    
+    // from add-item page
+    @PostMapping("/additem")
+    public String addItem(@Valid Item item, BindingResult result, Model model){
+        if (result.hasErrors()){
+            return "add-item";
+        }
+        itemRepository.save(item);
+        model.addAttribute("items", itemRepository.findAll());
+        return "items";
+    }
+    // from update-item
+    @PostMapping(value="/updateitem/{itemId}")
+    public String updateItem(@PathVariable("itemId") long itemId, @Valid Item item,
+    BindingResult result, Model model) {
+        if (result.hasErrors()){
+            item.setItemId(itemId);
+            return "update-item";
+        }
+        itemRepository.save(item);
+        model.addAttribute("items", itemRepository.findAll());
+        return "items";
+    }
+    
 }
