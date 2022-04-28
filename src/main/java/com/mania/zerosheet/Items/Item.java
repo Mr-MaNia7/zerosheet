3f54eca1ffd04d157ba21f873e04ac943dabfe92
Mr-MaNia7 <93995267+Mr-MaNia7@users.noreply.github.com>
2022-04-28 15:21:59 +0300

    Create relationship with Transaction class

--- src/main/java/com/mania/zerosheet/Items/Item.java
+++ src/main/java/com/mania/zerosheet/Items/Item.java
@@ -1,11 +1,16 @@
 package com.mania.zerosheet.Items;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
+
+import com.mania.zerosheet.Transaction.Transaction;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +35,10 @@ public class Item {
     private int totalQuantity;
     // @NotBlank(message = "Status category can not be Blank!")
     private Status category;
+
+    // Inverse side - One side
+    @OneToMany(mappedBy = "item")
+    Set<Transaction> transactions;
 
     // manual constructors for the commandlinerunner
     public Item(String itemName, String unit, double unitPrice, int totalQuantity, Status category){
