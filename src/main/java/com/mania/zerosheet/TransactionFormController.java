65b0cd6352e77ff084859365aed61a7f0e772a7f
Mr-MaNia7 <93995267+Mr-MaNia7@users.noreply.github.com>
2022-05-08 21:23:31 +0300

    Calculate day difference

--- src/main/java/com/mania/zerosheet/TransactionFormController.java
+++ src/main/java/com/mania/zerosheet/TransactionFormController.java
@@ -1,5 +1,7 @@
 package com.mania.zerosheet;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 import javax.validation.Valid;
 import com.mania.zerosheet.Customers.Customer;
 import com.mania.zerosheet.Items.ItemRepository;
@@ -24,6 +26,7 @@ public class TransactionFormController {
         model.addAttribute("items", itemRepository.findAll());
         return "Forms/item-transaction";
     }
+
     @ModelAttribute(name = "customer")
     public Customer order() {
         return new Customer();
@@ -39,8 +42,39 @@ public class TransactionFormController {
         if (result.hasErrors()) {
             return "Forms/item-transaction";
         }
+        // Calcualting Logic
+        // calculating date difference
+        Date fromDate = transaction.getDueBackDate();
+        Date toDate = transaction.getDueDate();        
+        long loan_days = calculateDayDifference(fromDate, toDate);
+        transaction.setDayDifference(loan_days);
+
+        // calculating loan price per transaction
+        double loan_price_per_day = transaction.getItem().getUnitLoanPrice();
+        double loan_price_per_item = loan_price_per_day * loan_days;
+        transaction.setTransPrice(transaction.getItemQuantity()*loan_price_per_item);
+        
+        // calculating collateral price per transaction
+        double collateral_price = transaction.getItem().getUnitPrice()*transaction.getItemQuantity();
+        transaction.setCollateral(collateral_price);
+
         order.addTransaction(transaction);
         // System.out.println(order.getTransactions()); // debug line
         return "redirect:/orders/current";
-    }   
+    }
+    
+    public long calculateDayDifference(Date fromDate, Date toDate) {
+        long difference_In_Time = fromDate.getTime() - toDate.getTime();
+        long difference_In_Days = 
+        TimeUnit
+              .MILLISECONDS
+              .toDays(difference_In_Time);
+
+        long difference_In_Years = 
+        TimeUnit
+              .MILLISECONDS
+              .toDays(difference_In_Time)
+          / 365l;
+        return difference_In_Days;
+    }
 }
