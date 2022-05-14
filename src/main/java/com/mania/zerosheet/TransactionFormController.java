18f7c69a7bbb24fa1d0a7a964d4e5449bdf383e0
Mr-MaNia7 <93995267+Mr-MaNia7@users.noreply.github.com>
2022-05-14 03:05:10 +0300

    Format file

--- src/main/java/com/mania/zerosheet/TransactionFormController.java
+++ src/main/java/com/mania/zerosheet/TransactionFormController.java
@@ -4,13 +4,16 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import javax.validation.Valid;
 import com.mania.zerosheet.Customers.Customer;
+import com.mania.zerosheet.Customers.CustomerRepository;
 import com.mania.zerosheet.Items.ItemRepository;
 import com.mania.zerosheet.Transaction.Transaction;
+import com.mania.zerosheet.Transaction.TransactionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,8 @@ import lombok.RequiredArgsConstructor;
 @SessionAttributes("customer")
 public class TransactionFormController {
     private final ItemRepository itemRepository;
+    private final TransactionRepository transactionRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/transactions/newtransaction")
     public String showTransactionForm(Model model) {
@@ -63,13 +68,14 @@ public class TransactionFormController {
         return "redirect:/orders/current";
     }
     
+    
     public long calculateDayDifference(Date fromDate, Date toDate) {
         long difference_In_Time = fromDate.getTime() - toDate.getTime();
         long difference_In_Days = 
         TimeUnit
               .MILLISECONDS
               .toDays(difference_In_Time);
-
+    
         long difference_In_Years = 
         TimeUnit
               .MILLISECONDS
