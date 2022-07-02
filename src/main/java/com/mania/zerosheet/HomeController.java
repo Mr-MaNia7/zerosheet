2612aa4dd5ad45551f8da46c690949edbff2ce72
Mr-MaNia7 <93995267+Mr-MaNia7@users.noreply.github.com>
2022-07-02 07:55:55 +0300

    Add contact controller

--- src/main/java/com/mania/zerosheet/HomeController.java
+++ src/main/java/com/mania/zerosheet/HomeController.java
@@ -1,23 +1,38 @@
 package com.mania.zerosheet;
 
+import com.mania.zerosheet.Customers.CustomerRepository;
 import com.mania.zerosheet.Items.Item;
 import com.mania.zerosheet.Items.ItemRepository;
+import com.mania.zerosheet.Transaction.TransactionRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
     private final ItemRepository itemRepository;
-    @Autowired
-    public HomeController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
+    private final CustomerRepository customerRepository;
+    private final TransactionRepository transactionRepository;
+    // @Autowired
+    // public HomeController(ItemRepository itemRepository) {
+    //     this.itemRepository = itemRepository;
+    //     this.
+    // }
     @GetMapping("/")
     public String showHomePage(Item item, Model model){
         model.addAttribute("items", itemRepository.findAll());
+        model.addAttribute("customers", customerRepository.findAll());
+        model.addAttribute("transactions", transactionRepository.findAll());
         return "index";
+    }
+    @GetMapping("/contact")
+    public String showContactPage(Model model){
+        // model.addAttribute(arg0, arg1)
+        return "Contact/contact";
     }
 }
