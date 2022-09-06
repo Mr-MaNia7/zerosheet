6bf0de29ccabf6b84e186fb59819e3e45862d76f
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-09-06 15:25:59 +0300

    Add duplicate-validation for customer

--- src/main/java/com/mania/zerosheet/OrderController.java
+++ src/main/java/com/mania/zerosheet/OrderController.java
@@ -20,18 +20,28 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
     private final CustomerRepository customerRepository;
     private final CompanyRepository companyRepository;
+    private boolean isDuplicate = false;
     
     @GetMapping("/orders/current")
     public String orderForm(Model model) {
         model.addAttribute("customers", customerRepository.findAll());
+        model.addAttribute("isDuplicate", isDuplicate);
         return "Forms/customer-info";
     }
     @PostMapping("/orders/agreement")
-    public String processOrder(@Valid Customer customer, 
+    public String processOrder(@Valid Customer customer,
     BindingResult result, SessionStatus status, Model model) {
         if (result.hasErrors()) {
             return "Forms/customer-info";
         }
+        customer.setFullName(customer.getName() + customer.getMiddleName() + customer.getLastName());
+        isDuplicate = false;
+        for (Customer cust : customerRepository.findAll()){
+            if (customer.getFullName().equals(cust.getFullName())){
+              isDuplicate = true;
+              return "redirect:/orders/current";
+            }
+          }
         customer.setOrderCost();
         model.addAttribute("customer", customer);
         model.addAttribute("transactions", customer.getPerformas());
@@ -39,4 +49,10 @@ public class OrderController {
         model.addAttribute("today", new Date());
         return "Agreements/view-agreement";
     }
+
+    @GetMapping("/cancel")
+    public String cancelTransaction(SessionStatus status){
+        status.setComplete();
+        return "redirect:/customers";
+  }
 }
