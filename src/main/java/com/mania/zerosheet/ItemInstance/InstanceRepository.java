d6ab44b498970898e7dc2c85ccf4eb3252157326
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-09-08 19:01:14 +0300

    Add find by status method

--- src/main/java/com/mania/zerosheet/ItemInstance/InstanceRepository.java
+++ src/main/java/com/mania/zerosheet/ItemInstance/InstanceRepository.java
@@ -1,9 +1,12 @@
 package com.mania.zerosheet.ItemInstance;
 
 import org.springframework.stereotype.Repository;
+import com.mania.zerosheet.ItemInstance.Instance.Status;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
 public interface InstanceRepository extends CrudRepository<Instance, Long>{
-    
+    List<Instance> findAllByStatus(Status status);
 }
